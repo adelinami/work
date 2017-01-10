ba69146a2e76eaa24cf3924a451e1af99032dc4a
adelinami <adelinami14@gmail.com>
2017-01-10 21:31:17 +0200

    Update test_mergesort.py

--- tests/test_mergesort.py
+++ tests/test_mergesort.py
@@ -7,16 +7,24 @@ from mergesort import merge_sort
 
 
 def test_happy_flow():
-    c = merge_sort([1,2,3], [2,3,4,9], 3, 4)
+    c = merge_sort([1,2,3], [2,3,4,9])
     assert c==[1,2,2,3,3,4,9]
 
 def test_happy_flow2():
-    c = merge_sort([1,2,3], [4,9], 3, 2)
+    c = merge_sort([1,2,3], [4,9])
     assert c==[1,2,3,4,9]
-
-def test_one_no_elements():
-    c = merge_sort([1,2,3], [], 3, 0)
+    
+def test_corner_case_a():
+    c = merge_sort([], [1,2,3])
     assert c==[1,2,3]
+
+def test_corner_case_b():
+    c = merge_sort([1,2,3], [])
+    assert c==[1,2,3]
+
+def test_corner_case_both():
+    c = merge_sort([], [])
+    assert c==[]
 
 # FIXME: good thing above - you checked a corner case - one of the lists is empty
 # ALWAYS check all corner cases - the other list is empty, both are empty

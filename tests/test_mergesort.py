b15466ba9e6b1f2ac6a3b7b931e71d3eb8644654
adelinami <adelinami14@gmail.com>
2017-01-10 22:05:57 +0200

    Update test_mergesort.py

--- tests/test_mergesort.py
+++ tests/test_mergesort.py
@@ -9,19 +9,15 @@ from mergesort import merge_sort
 def test_happy_flow():
     c = merge_sort([1,2,3], [2,3,4,9])
     assert c==[1,2,2,3,3,4,9]
-
-def test_happy_flow2():
-    c = merge_sort([1,2,3], [4,9])
-    assert c==[1,2,3,4,9]
     
-def test_corner_case_a():
+def test_first_list_empty():
     c = merge_sort([], [1,2,3])
     assert c==[1,2,3]
 
-def test_corner_case_b():
+def test_second_list_empty():
     c = merge_sort([1,2,3], [])
     assert c==[1,2,3]
 
-def test_corner_case_both():
+def test_both_lists_empty():
     c = merge_sort([], [])
     assert c==[]

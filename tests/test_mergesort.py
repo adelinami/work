9085d51ff12c3e48ae15b3d5ec039ce1a1dd6676
adelinami <adelinami14@gmail.com>
2017-01-12 22:50:49 +0200

    Update test_mergesort.py

--- tests/test_mergesort.py
+++ tests/test_mergesort.py
@@ -1,6 +1,6 @@
 import pytest
 
-from mergesort import merge_sort, merge_sort_13
+from mergesort import merge_sort_13
 
 def test_allempty():
     new_list=[]
@@ -9,11 +9,7 @@ def test_allempty():
         new_list=merge_sort_13(new_list,tuple[position])
         assert new_list==[]
 
-
-
-# tests for 'mergesort'
-
-
+"""
 def test_happy_flow():
     c = merge_sort([1,2,3], [2,3,4,9])
     assert c==[1,2,2,3,3,4,9]
@@ -29,3 +25,4 @@ def test_second_list_empty():
 def test_both_lists_empty():
     c = merge_sort([], [])
     assert c==[]
+"""

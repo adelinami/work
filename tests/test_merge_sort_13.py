344a17e9fadd0adb30c85b2639d5c2658e6e1482
adelinami <adelinami14@gmail.com>
2017-01-12 15:05:30 +0200

    Update test_merge_sort_13.py

--- tests/test_merge_sort_13.py
+++ tests/test_merge_sort_13.py
@@ -1,7 +1,7 @@
 
 import pytest
 
-from mergesort.mergesort_for_13_lists import merge_sort
+from mergesort import merge_sort_13
 
 
 # tests for 'mergesort_for_13_lists'
@@ -10,5 +10,5 @@ def test_allempty():
     new_list=[]
     tuple = [], [], [], [], [], [], [], [], [], [], [], [], []
     for position in range( 13):
-        new_list=merge_sort(new_list,tuple[position])
+        new_list=merge_sort_13(new_list,tuple[position])
         assert new_list==[]
